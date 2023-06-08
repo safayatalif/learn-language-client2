e5528336e04c78aa330b1937259c28b234510f3f
safayatalif <safayatalif2021@gmail.com>
2023-06-08 22:39:11 +0600

    lenguage category data fetch and install react tabs and implement

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 
 const Classes = () => {
+    const [language , setLanguage] = useState("English")
+    const [classItems, setClassItem] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/classes/${language}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setClassItem(data);
+            });
+    }, [language])
+
+    console.log(classItems)
+
     return (
         <div className="pt-20">
-            classes page 
+            <Tabs>
+                <TabList>
+                    <Tab onClick={() => setLanguage('English')}>
+                        <button className="btn btn-primary btn-outline">English</button>
+                    </Tab>
+                    <Tab onClick={() => setLanguage('Spanish')}>
+                        <button className="btn btn-primary btn-outline">Spanish</button>
+                    </Tab>
+                    <Tab onClick={() => setLanguage('Japanese')}>
+                        <button className="btn btn-primary btn-outline">Japanese</button>
+                    </Tab>
+                    <Tab onClick={() => setLanguage('French')}>
+                        <button className="btn btn-primary btn-outline">French</button>
+                    </Tab>
+                    <Tab onClick={() => setLanguage('Chinese')}>
+                        <button className="btn btn-primary btn-outline">Chinese</button>
+                    </Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 4</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 5</h2>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
